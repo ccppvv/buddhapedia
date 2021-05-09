a474283794d246c51b00b6ad8d3f0cc20eddb13a
ccppvv <ccppvv@hotmail.com>
2021-05-09 10:31:01 +0800

    feat: 查词的词典参数改为dictId

--- app/controller/words.js
+++ app/controller/words.js
@@ -5,7 +5,7 @@ const Controller = require('egg').Controller;
 class WordsController extends Controller {
   async index() {
     const ctx = this.ctx;
-    let { word, dictid, pageSize = 1, pageIndex = 20 } = ctx.query;
+    let { word, dictId, pageSize = 1, pageIndex = 20 } = ctx.query;
     pageSize = parseInt(pageSize);
     pageIndex = parseInt(pageIndex);
     if (pageSize <= 0 || isNaN(parseInt(pageSize)) || pageIndex <= 0) {
@@ -15,7 +15,7 @@ class WordsController extends Controller {
       };
       return;
     }
-    const queries = { word, dictid, pageSize, pageIndex };
+    const queries = { word, dictid: dictId, pageSize, pageIndex };
     ctx.body = await this.ctx.service.words.list(queries);
   }
 }
